ca0019e8a349c0329c7607c6c948470d967861d0
Astrid Van Camp <astrid.vancamp@hotmail.com>
2020-04-22 12:22:07 +0200

    Data split segmentation

--- InitData.py
+++ InitData.py
@@ -26,7 +26,7 @@ class_list = class_file.read().splitlines()
 # Read list of all files for segmentation
 segm_file = open(os.path.join(folder, 'ImageSets', 'Segmentation', 'trainval.txt'))
 segm_list = segm_file.read().splitlines()
-# Split data for training, validation and testing
+# Split data
 segm_train_list, segm_valtest_list = train_test_split(segm_list, test_size=0.5, random_state=42)
 segm_val_list, segm_test_list = train_test_split(segm_valtest_list, test_size=0.5, random_state=42)
 
